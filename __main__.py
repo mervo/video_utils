2874d31fe3fd9c9639a6f9012d71693dba284d5b
mervo <9879403+mervo@users.noreply.github.com>
2020-10-06 16:23:34 +0800

    Working multi-stream imshow and recording.

--- __main__.py
+++ __main__.py
@@ -12,19 +12,22 @@ if __name__ == '__main__':
     #                           isVideoFile=False, queueSize=2, writeDir='./output',
     #                           reconnectThreshold=1, max_height=1080, method='cv2')
 
-    vidManager = VideoManager(camNames='File'.split(','),
-                              streams='/data/datasets/drone/macritchie-reservoir.mp4'.split(','),
-                              isVideoFile=True, queueSize=2, writeDir=None,
+    vidManager = VideoManager(camNames=['File', 'RTSP'],
+                              streams=['/data/datasets/drone/macritchie-reservoir.mp4',
+                                       'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov'],
+                              isVideoFile=True, queueSize=2, writeDir='./output',
                               reconnectThreshold=1, max_height=1080, method='cv2')
 
     vidManager.start()
-    vidInfos = vidManager.getAllInfo()
-    print(f'vid infos: {vidInfos}')
+    print(f'Sources: {vidManager.videos} Details: {vidManager.getAllInfo()}')
 
     for frame_count in itertools.count():
         statuses, frames = vidManager.read()  # frames is list of arrays from 0 - 255, dtype uint8
-        if len(frames[0]) != 0:
-            cv2.imshow('Preview', frames[0])
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-    cv2.destroyAllWindows()
+        for i, video_stream_information in enumerate(vidManager.videos):
+            if len(frames[i]) != 0:
+                cv2.imshow(video_stream_information['camName'], frames[i])
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+    vidManager.stop()
