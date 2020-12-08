45b10a61235aca54f34810e61c93ca17bd864a06
levan92 <lingevan0208@gmail.com>
2020-12-08 15:03:05 +0800

    impose max size in frames deque

--- video_getter_cv2.py
+++ video_getter_cv2.py
@@ -32,6 +32,7 @@ class VideoStream:
                  resize_fn=None,
                  frame_crop=None,
                  rtsp_tcp=True,
+                 max_cache=10,
                  logger=None):
 
         if logger is None:
@@ -48,6 +49,7 @@ class VideoStream:
         self.pauseTime = None
         self.stopped = True
         self.Q = deque(maxlen=queue_size)  # Maximum size of a deque or None if unbounded.
+        self.max_cache = max_cache
         self.resize_fn = resize_fn
         self.inited = False
         if (manual_video_fps == -1):
@@ -128,6 +130,10 @@ class VideoStream:
         while not self.stopped:
             try:
                 # print('getting video' + str(time.time()))
+                if len(self.Q) > self.max_cache:
+                    time.sleep(0.01)
+                    continue
+
                 grabbed, frame = self.stream.read()
 
                 if grabbed:
